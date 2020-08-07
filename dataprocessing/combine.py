0de72a63994496c47a56e5c23a8e01620f7d7582
龚佳俊 <jgongac@connect.ust.hk>
2020-08-07 21:04:15 +0800

    combine function

--- dataprocessing/combine.py
+++ dataprocessing/combine.py
@@ -3,54 +3,41 @@ import os
 from os import makedirs
 from os.path import join, abspath, dirname, pardir
 import glob
-import re
 import argparse
 import time
-import numpy as np
 Pardir = abspath(join(dirname(__file__), pardir))
-DumpDir = join( Pardir , "dataset")
+
+DumpDir = join( Pardir , "parsed")
 def parse_arguments():
 
-	parser = argparse.ArgumentParser(description='Combine parsed datasets')
+	parser = argparse.ArgumentParser(description='Crawl Alexa top websites and capture the traffic')
 
-	parser.add_argument('-dir',
+	parser.add_argument('--dir','-d',
 						nargs='+',
 						type=str,
 						metavar='<batch dir>',
 						dest = 'dirlist',
 						default = [],
 						help='bacth folders')
-	parser.add_argument('-mode',
-						type=str,
-						metavar='<mode>',
-						default='mon',
-						help='monitor or unmonitor:mon, unmon')	
-	parser.add_argument('-dataset',
-						type=str,
-						metavar='<dataset type>',
-						help='clean or defended?:clean, dp, tamaraw')	
-	parser.add_argument('-t',
-						type=str,
-						metavar='<T>',
-						default='20',
-						help='param T')	
-	parser.add_argument('-e',
-						type=str,
-						metavar='<epsilon>',
-						default='05',
-						help='param epsilon')	
-	parser.add_argument('-l',
-						type=str,
-						metavar='<L>',
-						default='100',
-						help='param L')	
-	parser.add_argument('-w',
-						type=str,
-						metavar='<W>',
-						default='6',
-						help='param W')	
-
-	parser.add_argument('-suffix',
+	parser.add_argument('--start','-s',
+						type=int,
+						metavar='<start ind>',
+						default=0,
+						help='Start from which site in the list (include this ind).')
+	parser.add_argument('--end','-e',
+						type=int,
+						metavar='<end ind>',
+						default=100,
+						help='End to which site in the list (exclude this ind).')
+	parser.add_argument('-c',
+						action='store_true',
+						default=False,
+						help='Keep a copy of original dataset? (default:False)')
+	parser.add_argument('-u',
+						action='store_true',
+						default=False,
+						help='is monitored webpage or unmonitored? (default:is monitored, False)')
+	parser.add_argument('--format','-f',
 						type=str,
 						metavar='<suffix>',
 						default='.cell',
@@ -60,69 +47,54 @@ def parse_arguments():
 	args = parser.parse_args()
 	return args
 
-def init_directories(mode, t, l, e, w):
-    # Create a results dir if it doesn't exist yet
-    if not os.path.exists(DumpDir):
-        makedirs(DumpDir)
+def init_directories(start,end, u):
+	# Create a results dir if it doesn't exist yet
+	if not os.path.exists(DumpDir):
+		makedirs(DumpDir)
+	if u:
+		prefix = "u"
+	else:
+		prefix = ""
+	# Define output directory
+	timestamp = time.strftime('%m%d_%H%M%S')
+	output_dir = join(DumpDir, prefix+'dataset'+str(start)+'_'+str(end-1)+'_'+timestamp)
+	makedirs(output_dir)
 
-    # Define output directory
-    # timestamp = time.strftime('%m%d_%H%M')
-    if args.dataset == 'clean':
-    	output_dir = join(DumpDir, mode+"_clean")
-    elif args.dataset == 'tamaraw':
-    	output_dir = join(DumpDir, mode+"_tamaraw")
-    elif args.dataset == 'wt':
-    	output_dir = join(DumpDir, mode+"_wt")
-    elif args.dataset == 'dp':
-    	output_dir = join(DumpDir, mode+"_T"+t+"_L"+l+"_E"+e+"_W"+w)
-    else:
-    	raise ValueError("Wrong dataset type!")
-    makedirs(output_dir)
-
-    return output_dir
+	return output_dir
 
 if __name__ == '__main__':
 	args = parse_arguments()
 	folders = args.dirlist
-	output_dir = init_directories(args.mode, args.t, args.l, args.e, args.w)
-	print("Mode:{}, Create fold {}".format(args.mode,output_dir))
-
-	cnt = {}
+	raw = []
 	for folder in folders:
-		files = glob.glob(join(folder, "*"+args.suffix))
-		for file in files:
-			if args.mode == 'mon':
+		raw += glob.glob(join(folder, "*"+args.format))
+	print("Total:{}".format(len(raw)))
+	output_dir = init_directories(args.start,args.end, args.u)
+	counter = [0]*args.end
+	# print(raw)
+	for r in raw:
+		filename = r.split("/")[-1].split(args.format)[0]
+		if args.u:
+			web_id = filename
+			newfilename = filename + args.format
+		else:
+			web_id,inst_id = filename.split("-")
+			new_inst_id = str(counter[int(web_id)])
+			newfilename = web_id + "-" + new_inst_id + args.format
+		if args.c:
+			command = "cp "
+		else:
+			command = "mv "
+		#move pcapfile and time file
+		cmd = command + r + " " +join(output_dir, newfilename)
+		subprocess.call(cmd, shell=True)
+		# cmd = command + r.split(".")[0]+".time" + " " +join(output_dir, newfilename.split(".")[0]+".time")
+		# print(cmd)
+		# subprocess.call(cmd, shell=True)
+		counter[int(web_id)] += 1
+	for i in range(len(counter)):
+		if counter[i] > 0 :
+			print("#{}:{}".format(i, counter[i]))
+	print("Merged to {}".format(output_dir))
 
-				tmp = file.split("/")[-1].split(args.suffix)[0]
-				label = int(tmp.split("-")[0])
-				inst  = int(tmp.split("-")[1])
-				if label in cnt.keys():
-					newinst = cnt[label]
-					cnt[label] += 1
-				else:
-					newinst = 0
-					cnt[label] = 1
 
-				newfiledir = join( output_dir, str(label) + '-' + str(newinst) +args.suffix)
-				cmd = "cp " + file + " " + newfiledir
-				subprocess.call(cmd, shell = True) 
-			else:
-				#unmon to do
-				pass 
-				# oldlabel = int(file.split("/")[-1].split(args.suffix)[0])
-				# if oldlabel in selected_list:
-				# 	newlabel = np.where(selected_list==oldlabel)[0][0]
-				# 	newfiledir = join( output_dir, str(newlabel) +args.suffix)
-				# 	cmd = "cp " + file + " " + newfiledir
-				# 	# print(cmd)
-				# 	subprocess.call(cmd, shell = True) 
-	
-	if args.mode == 'unmon':
-		#to do
-		pass
-		# tmp = output_dir.rstrip("/").split("/")[-1]
-		# mon_dir = join(targetdir, "mon_" + tmp.split("unmon_")[-1])
-		# cmd = "mv " + join(output_dir, "*") +  " " + mon_dir
-		# # print(cmd)
-		
-		# subprocess.call(cmd,shell=True)
